ff545dbe76258b0110e2038aef4e146cefdc3343
Aleksandrs Livincovs <aleksandrs.livincovs@gmail.com>
2016-05-04 14:19:28 +0200

    travis setup

--- scripts/debian/build_deb.sh
+++ scripts/debian/build_deb.sh
@@ -4,6 +4,6 @@ ROOT=./
 SCRIPTS=scripts/debian
 git describe --tags | sed 's/-\(alpha\|beta\|rc\)/~\1/' | cut -c2- > VERSION
 chmod 0775 ${SCRIPTS}/dpkg-root/DEBIAN/*
-cp ${SCRIPTS]}/Makefile ${ROOT}
+cp ${SCRIPTS}/Makefile ${ROOT}
 make clean deb
 
