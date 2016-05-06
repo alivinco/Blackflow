e4d380608b227cceec990779c80f3035b22b67fa
Aleksandrs Livincovs <aleksandrs.livincovs@gmail.com>
2016-05-06 11:06:16 +0200

    msg timestamp format change . Added the service startup script .

--- scripts/start_blackflow.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd ../
-export PYTHONPATH=./
-python blackflow/bin/blackflow_service.py -c examples/config/blackflow_config.json -a apps
---
+++ start_blackflow.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export PYTHONPATH=./
+python blackflow/bin/blackflow_service.py -c examples/config/blackflow_config.json -a ./../BlackflowApps/apps
